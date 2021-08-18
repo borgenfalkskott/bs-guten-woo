b926f120eaf269650631ef4d3d128e361f24e30f
tommyskott <tommy@borgenfalk.se>
2021-08-19 00:10:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# B&S - Guten Woo
+
+Enable the gutenberg editor for your WooCommerce products.
---
+++ bs-guten-woo.php
@@ -0,0 +1,61 @@
+<?php
+  /*
+    Plugin Name:  B&S - Guten Woo
+    Plugin URI:   https://github.com/borgenfalkskott/
+    Description:  Enable the gutenberg editor for your WooCommerce products.
+    Version:      0.1.0
+    Author:       Borgenfalk & Skott
+    Author URI:   https://borgenfalk.se
+    License:      MIT
+    License URI:  https://github.com/borgenfalkskott/
+    Text Domain:  bs-guten-woo
+    Domain Path:  /languages
+  */
+
+  defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
+  /**
+   * Enable the Gutenberg Editor for your WooCommerce Products
+   */
+  add_filter( 'use_block_editor_for_post_type', function( $can_edit, $post_type ){
+    if( $post_type == 'product' ) $can_edit = true;
+    return $can_edit;
+  }, 22, 2);
+
+
+  /**
+   * Disable Woo product tabs
+   */
+  add_filter('woocommerce_product_tabs', function( $tabs ){
+    unset( $tabs['description'] );
+    unset( $tabs['reviews'] );
+    unset( $tabs['additional_information'] );
+    return $tabs;
+  }, 99);
+
+
+  /**
+   * Render single product description/content
+   */
+  add_action('woocommerce_after_single_product_summary', function(){
+    wc_get_template('single-product/tabs/description.php');
+  }, 99);
+
+
+  /**
+   * Remove description heading on product page
+   */
+  add_filter('woocommerce_product_description_heading', '__return_null');
+
+
+  /**
+   * Enable Woo taxonomies in Wp REST API
+   */
+  function bs_guten_woo_show_taxonomy_in_api( $args ) {
+    $args['show_in_rest'] = true;
+    return $args;
+  }
+  add_filter('woocommerce_taxonomy_args_product_cat', 'bs_guten_woo_show_taxonomy_in_api');
+  add_filter('woocommerce_taxonomy_args_product_tag', 'bs_guten_woo_show_taxonomy_in_api');
+?>
